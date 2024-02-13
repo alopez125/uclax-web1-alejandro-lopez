25c9bd45f7e36fb4460423b38996f40bcfa54fcb
Alejandro Lopez <alejandro7designs@gmail.com>
2024-02-12 18:53:41 -0800

    L05 Complete

---
+++ src/Common/PagesLayout/Header/Header.jsx
@@ -0,0 +1,25 @@
+/** @format */
+
+import styled from "styled-components";
+
+//Media Query
+
+import { useMediaQuery } from "@/Common/useMediaQuery";
+
+//components
+import MediumLarge from "./MediumLarge";
+import Small from "./Small";
+
+const Header = () => {
+    const { isMediumAndUp } = useMediaQuery();
+
+    return (
+        <HeaderStyled>
+            {isMediumAndUp ? <MediumLarge /> : <Small />}
+        </HeaderStyled>
+    );
+};
+
+export default Header;
+
+const HeaderStyled = styled.header``;
